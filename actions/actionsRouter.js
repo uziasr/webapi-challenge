f5ece7df01bdf3e63764979b68e31e73d2967c42
Uzias Rivera <uziasrivera@gmail.com>
2019-11-01 10:45:12 -0700

    completed project

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -12,32 +12,23 @@ router.get('/projects/actions', (req,res)=>{
     })
 })
 
-// router.get('/actions/:id', (req,res)=>{
-//     const id = req.params.id
-//     actionDB.getProjectActions(id)
-//     .then(action=>res.status(200).json(action))
-//     .catch(err=>{
-//         console.log(err)
-//         res.status(500).json({error: 'something went terribly wrong!'})
-//     })
-// })
-
-router.post('/projects/:id/actions', (req,res)=>{
+router.post('/projects/:id/actions', validateAction, (req,res)=>{
     // required --> project_id, description, notes
     const newAction = req.body
     const projectId = req.params.id
     actionDB.insert({...newAction, project_id:projectId})
     .then(newOne=>{
-        (newOne.length!==0)?res.status(200).json(newOne):res.status(404).json({error:'make sure that id is valid for me'})
+        // console.log(newOne);
+        (newOne.length!==0)? res.status(200).json(newOne) : res.status(404).json({error: 'something went terribly wrong!'})
     })
     .catch(err=>{
         console.log(err)
-        res.status(500).status.json({error: 'something went wrong'})
+        res.status(500).json({error: 'something went wrong, make sure project id is valid'})
     })
 
 })
 
-router.put('/projects/actions/:id', (req,res)=>{
+router.put('/projects/actions/:id', validateAction, (req,res)=>{
     const actionID = req.params.id
     const updatedAction = req.body
     actionDB.update(actionID, updatedAction)
@@ -49,5 +40,26 @@ router.put('/projects/actions/:id', (req,res)=>{
         res.status(500).json({error:"something went wrong!"})
     })
 })
+
+router.delete('/projects/actions/:id', (req,res)=>{
+    const actionID = req.params.id
+    console.log(actionID)
+    actionDB.remove(actionID)
+    .then(count=>{
+        count? res.status(200).json({success:`action deleted with the id of ${actionID}`}):
+        res.status(404).json({error:"no actions were deleted, check the id"})
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({error:"something went wrong!"})
+    })
+})
+
+function validateAction(req, res, next){
+    // console.log(req.body)
+    const objLength = (Object.keys(req.body).length)
+   return (!objLength)?res.status(404).json({error:"please send something"}):('notes' in req.body & 'description' in req.body)? next(): res.status(404).json({error: "please make sure description and notes are being sent "})
+}
+
 
 module.exports = router;
