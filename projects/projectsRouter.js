3dac9797568dd570093c3e2c5a402935e4ada257
Uzias Rivera <uziasrivera@gmail.com>
2019-11-01 09:35:24 -0700

    delete and update completed for projects!

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -25,5 +25,32 @@ router.post('/projects', (req,res)=>{
     })
 })
 
+router.put('/projects/:id', (req,res)=>{
+    //working
+    const id = req.params.id
+    const updatedProject = req.body
+    projectDB.update(id, updatedProject)
+    .then(updated=>{
+        updated? res.status(200).json(updatedProject):res.status(404).json({error:'id not found'})
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({error:'something went wrong'})
+    })
+})
+
+router.delete('/projects/:id', (req,res)=>{
+    const id = req.params.id
+    projectDB.remove(id)
+    .then(count=>{
+        count? res.status(200).json({success:`project with the id of ${id} has been removed!`}):res.status(404).json({error:'please double check that the id exist, no records were removed'})
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({error:'something went wrong'})
+    })
+
+})
+
 
 module.exports= router;
